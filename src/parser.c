9032f4eaae919e04da873e2d9f349e19be6c65a4
Monkvy <moluecke@gmx.de>
2023-08-17 23:37:39 +0200

    comments changed

--- src/parser.c
+++ src/parser.c
@@ -16,7 +16,6 @@ find_keyword(const char *keyword)
 int32_t 
 match_punct(const char *source, int32_t i, uint32_t *punct_len)
 {
-	// TODO: error occurs on this fn.
 	uint32_t punctuations_len = sizeof(punctuations) / sizeof(punctuations[0]);
 
 	uint32_t punct_longest = 0;
@@ -35,7 +34,7 @@ match_punct(const char *source, int32_t i, uint32_t *punct_len)
 		if (strlen(punctuations[punct_i]) != *punct_len)
 			continue;
 
-		/* Copy the substring from i to i + punct_len */
+		/* Copy the substring source[i] to source[i + punct_len] */
 		char *substr = malloc(*punct_len + 1);
 		strncpy(substr, source + i, *punct_len);
 		substr[*punct_len] = '\0';
@@ -60,8 +59,8 @@ lex_next(const char *source, int32_t *i, struct Token *token)
 	
 	if (is_digit(source[*i]))
 	{
-		// Loop over all digit chars in source code
-		// (the index `i` will be on the last valid digit char)
+		/* Loop over all digit chars in source code 
+		(the index `i` will be on the last valid digit char) */
 		int32_t start_i = *i;
 		while (is_digit(source[*i + 1]))
 			++(*i);
@@ -73,7 +72,7 @@ lex_next(const char *source, int32_t *i, struct Token *token)
 		strncpy(value, source + start_i, *i - start_i + 1);
 		value[*i - start_i + 1] = '\0';
 
-		// currently, `i` is the last valid digit char so add one
+		/* currently, `i` is the last valid digit char so add one */
 		++(*i);
 
 		token->type = TT_LITERAL;
@@ -86,8 +85,8 @@ lex_next(const char *source, int32_t *i, struct Token *token)
 
 	if (is_ident_start(source[*i]))
 	{
-		// Loop over all valid identifier chars in source code
-		// (the index `i` will be on the last valid ident char)
+		/* Loop over all valid identifier chars in source code
+		(the index `i` will be on the last valid ident char) */
 		int32_t start_i = (*i)++;
 		
 		while (is_ident_char(source[*i + 1]))
@@ -97,11 +96,11 @@ lex_next(const char *source, int32_t *i, struct Token *token)
 		if (ident == NULL)
 			return (struct Error){ ERROR_MEMORY_ALLOCATION, "malloc failed" };
 
-		// copy content from start_i to current i to `ident`
+		/* copy content from start_i to current i to `ident` */
 		strncpy(ident, source + start_i, *i - start_i + 1);
 		ident[*i - start_i + 1] = '\0';
 
-		// currently, `i` is the last valid ident char so add one
+		/* currently, `i` is the last valid ident char so add one */
 		++(*i);
 
 		int32_t kw_i;
