88d8b4a037ceb9a19471960a7505f1178f82f304
Monkvy <moluecke@gmx.de>
2023-08-18 00:03:04 +0200

    formatted

--- src/main.c
+++ src/main.c
@@ -23,15 +23,18 @@ print_help()
 int32_t
 main(int argc, char **argv)
 {
-	if (argc == 1) {
+	if (argc == 1)
+	{
 		fprintf(stderr, "command expected");
 		return -1;
 	}
-	if (!strcmp(argv[1], "help")) {
+	if (!strcmp(argv[1], "help"))
+	{
 		print_help();
 		return 0;
 	}
-	else if (!strcmp(argv[1], "file")) {
+	else if (!strcmp(argv[1], "file"))
+	{
 		const char *source = read_file(argv[2]);
 		int32_t i = 0;
 
@@ -40,18 +43,19 @@ main(int argc, char **argv)
 			struct Token token;
 			struct Error err = lex_next(source, &i, &token);
 
-			if (err.code != ERROR_NONE) {
+			if (err.code != ERROR_NONE)
+			{
 				free((void*)source);
 				return fail(err);
 			}
-
 			printf("%i, %s\n", token.type, token.value);
 		}
 
 		free((void*)source);
 		return 0;
 	}
-	else if (!strcmp(argv[1], "buffer")) {
+	else if (!strcmp(argv[1], "buffer"))
+	{
 		/* const char* source = argv[2]; */
 		// TODO: pass source into lexer (or tokenizer)
 		return 0;
