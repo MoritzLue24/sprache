85019a63123a4845394eb6141ba1168835199f55
Monkvy <moluecke@gmx.de>
2023-08-21 20:33:55 +0200

    ast

---
+++ src/ast.h
@@ -0,0 +1,52 @@
+#ifndef AST_H
+#define AST_H
+
+#include <stdint.h>
+
+
+enum NodeKind
+{
+	NODE_ROOT,
+	NODE_FUNCTION,
+	NODE_RETURN,
+	NODE_LITERAL,
+};
+
+struct Node;
+
+struct Node_Root
+{
+	struct Node *body;
+};
+
+struct Node_Function
+{
+	const char *name;
+};
+
+struct Node_Return 
+{
+	Node *value;
+};
+
+struct Node_Literal 
+{
+	struct Node *body;
+	uint32_t u_value;
+};
+
+struct Node 
+{
+	enum NodeKind kind;
+	int32_t start_i;
+	int32_t end_i;
+
+	union {
+		struct Node_Root root;
+	};
+};
+
+
+
+
+#endif
